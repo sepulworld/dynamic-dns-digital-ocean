746dd729e05e804091b2f7b9aeb61e9ce8b57510
zane <zane@ugh.cloud>
2020-05-07 22:36:27 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# dynamic-dns-digital-ocean
+
+Simple Docker supported dynamic DNS updater for DNS hosted on Digital Ocean 
+
+Run on a Docker host in your home :)
+
+### Requirements
+
+* Digital Ocean API Token
+* Your domain's name server must be pointing to Digital Ocean NS for your domain
+
+
+### Docker Compose
+
+Update docker compose with your Digital Token and run
+
+```
+docker-compose up -d
+```
---
+++ code/ddns-do.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import click
+import digitalocean
+import requests
+import os
+import time
+import tldextract
+
+AUTH_TOKEN = os.environ.get("DIGITAL_OCEAN_AUTH_TOKEN", "")
+
+
+@click.command()
+@click.option('--check-interval', '-i', default=300)
+@click.option('--domain', '-d', multiple=True, required=True)
+@click.option(
+    '--digital-ocean-auth-token',
+    '-t',
+    default=AUTH_TOKEN,
+    required=True
+)
+def run(check_interval,
+        digital_ocean_auth_token,
+        domain):
+    """Run process to monitor and update DNS"""
+    while True:
+        for d in domain:
+            ip = _get_ip()
+            print(ip)
+            tld, subdomain = _extract_domain_and_subdomain(d)
+            _set_dns(tld, subdomain, ip, digital_ocean_auth_token)
+            time.sleep(check_interval)
+
+
+def _get_ip():
+    """Fetch my IP address as seen from outside world"""
+    ip = requests.get("https://api.ipify.org/?format=json").json()['ip']
+    return ip
+
+
+def _extract_domain_and_subdomain(domain):
+    subdomain = None
+    extract = tldextract.extract(domain)
+    tld = extract.domain + "." + extract.suffix
+    if extract.subdomain:
+        subdomain = extract.subdomain
+
+    return tld, subdomain
+
+
+def _set_dns(tld, subdomain, ip, token):
+    domain = digitalocean.Domain(token=token, name=tld)
+    records = domain.get_records()
+    for r in records:
+        if r.name == subdomain:
+            print(f"Updating {subdomain} to {ip}")
+            r.data = ip
+            r.ttl = 60
+            r.save()
+            break
+    else:
+        print(f"Creating new {subdomain} with A: {ip}")
+        domain.create_new_domain_record(
+            type='A',
+            name=subdomain,
+            data=ip
+            )
+
+
+if __name__ == '__main__':
+    run()
